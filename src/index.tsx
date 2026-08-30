4551fcd2616e3746c9de7932341fa68644ca4e46
Ocupuc123 <ocupuc123@mail.ru>
2026-08-30 22:14:18 +0300

    feat: добавит компоненты экранов

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+
+const rentalOffersCount = 5;
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
-  </React.StrictMode>
+    <App rentalOffersCount={rentalOffersCount} />
+  </React.StrictMode>,
 );
